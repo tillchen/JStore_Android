67c369dbff6874f431e01046ca92f2d91f947e56
tillchen <ti.chen@jacobs-university.de>
2019-10-09 15:18:11 +0200

    enabled anonymous sign-in

---
+++ app/src/main/java/com/tillchen/jstore/AnonymousNewUserActivity.java
@@ -0,0 +1,13 @@
+package com.tillchen.jstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnonymousNewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anonymous_new_user);
+    }
+}
