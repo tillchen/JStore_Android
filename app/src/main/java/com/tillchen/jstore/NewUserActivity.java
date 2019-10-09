0f1a5f0339ce6d4fcd49e600f2a603765a6d9a5f
tillchen <ti.chen@jacobs-university.de>
2019-10-09 18:30:36 +0200

    minor update for NewUserActivity

--- app/src/main/java/com/tillchen/jstore/NewUserActivity.java
+++ app/src/main/java/com/tillchen/jstore/NewUserActivity.java
@@ -5,17 +5,22 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
-public class NewUserActivity extends AppCompatActivity implements View.OnClickListener {
+public class NewUserActivity extends UtilityActivity implements View.OnClickListener {
 
     RadioButton mRadioButton1; // WhatsApp
     RadioButton mRadioButton2; // Email
     Button mButton; // Start Using Button
-    EditText mEditText; // phone
+    EditText mEditTextPhone;
+    EditText mEditTextFullName;
+    String mFullName; // the full name that the user entered
+    String mPhone; // the phone number that the user entered
+    boolean isWhatsApp = true; // whether the user selected WhatsApp
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +33,31 @@ public class NewUserActivity extends AppCompatActivity implements View.OnClickLi
         mRadioButton1 = findViewById(R.id.WhatsApp_radioButton);
         mRadioButton2 = findViewById(R.id.email_radioButton);
         mButton = findViewById(R.id.start_button_1);
-        mEditText = findViewById(R.id.phone_editText);
+        mEditTextPhone = findViewById(R.id.phone_editText);
+        mEditTextFullName = findViewById(R.id.full_name_editText);
 
         mRadioButton1.setOnClickListener(this);
         mRadioButton2.setOnClickListener(this);
         mButton.setOnClickListener(this);
 
-        mEditText.setVisibility(View.VISIBLE);
+        mEditTextPhone.setVisibility(View.VISIBLE);
+
+        mEditTextFullName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+        mEditTextPhone.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
     }
 
     @Override
@@ -42,18 +65,36 @@ public class NewUserActivity extends AppCompatActivity implements View.OnClickLi
         switch (v.getId()) {
             case R.id.WhatsApp_radioButton:
                 if (((RadioButton) v).isChecked()) {
-                    mEditText.setVisibility(View.VISIBLE);
+                    mEditTextPhone.setVisibility(View.VISIBLE);
+                    isWhatsApp = true;
                 }
                 break;
             case R.id.email_radioButton:
                 if (((RadioButton) v).isChecked()) {
-                    mEditText.setVisibility(View.INVISIBLE);
+                    mEditTextPhone.setVisibility(View.INVISIBLE);
+                    isWhatsApp = false;
                 }
                 break;
             case R.id.start_button_1:
+                mFullName = mEditTextFullName.getText().toString();
+                mPhone = mEditTextPhone.getText().toString();
+
+                if (TextUtils.isEmpty(mFullName)) {
+                    mEditTextFullName.setError("Full Name can't be empty.");
+                    return;
+                }
+
+                if (isWhatsApp && TextUtils.isEmpty(mPhone)) {
+                    mEditTextPhone.setError("Phone number can't be empty.");
+                    return;
+                }
+
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 // TODO: 0 SAVE the info
+                if (isWhatsApp) { // TODO: 0 Save the phone number
+                    
+                }
                 finish();
                 break;
             default:
