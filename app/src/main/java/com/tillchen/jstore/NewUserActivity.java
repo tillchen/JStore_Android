6c5728a9489ac339ef978f171a5f510110cb51ba
tillchen <ti.chen@jacobs-university.de>
2019-10-09 17:52:42 +0200

    minor update for NewUserActivity

--- app/src/main/java/com/tillchen/jstore/NewUserActivity.java
+++ app/src/main/java/com/tillchen/jstore/NewUserActivity.java
@@ -3,9 +3,19 @@ package com.tillchen.jstore;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
-public class NewUserActivity extends AppCompatActivity {
+public class NewUserActivity extends AppCompatActivity implements View.OnClickListener {
+
+    RadioButton mRadioButton1; // WhatsApp
+    RadioButton mRadioButton2; // Email
+    Button mButton; // Start Using Button
+    EditText mEditText; // phone
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +24,41 @@ public class NewUserActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar_login);
         setSupportActionBar(toolbar);
+
+        mRadioButton1 = findViewById(R.id.WhatsApp_radioButton);
+        mRadioButton2 = findViewById(R.id.email_radioButton);
+        mButton = findViewById(R.id.start_button_1);
+        mEditText = findViewById(R.id.phone_editText);
+
+        mRadioButton1.setOnClickListener(this);
+        mRadioButton2.setOnClickListener(this);
+        mButton.setOnClickListener(this);
+
+        mEditText.setVisibility(View.VISIBLE);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.WhatsApp_radioButton:
+                if (((RadioButton) v).isChecked()) {
+                    mEditText.setVisibility(View.VISIBLE);
+                }
+                break;
+            case R.id.email_radioButton:
+                if (((RadioButton) v).isChecked()) {
+                    mEditText.setVisibility(View.INVISIBLE);
+                }
+                break;
+            case R.id.start_button_1:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                // TODO: 0 SAVE the info
+                finish();
+                break;
+            default:
+                break;
+        }
+    }
+
 }
