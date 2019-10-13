568a0ae49d7722417239235806267583a27b806a
tillchen <ti.chen@jacobs-university.de>
2019-10-13 14:55:54 +0200

    added toolbar and scrollView for BuyFragment

--- app/src/main/java/com/tillchen/jstore/ui/buy/BuyFragment.java
+++ app/src/main/java/com/tillchen/jstore/ui/buy/BuyFragment.java
@@ -4,31 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.tillchen.jstore.R;
 
 public class BuyFragment extends Fragment {
 
-    private BuyViewModel buyViewModel;
+    private Toolbar mToolbar;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        buyViewModel = ViewModelProviders.of(this).get(BuyViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_buy, container, false);
 
-        final TextView textView = root.findViewById(R.id.text_buy);
-        buyViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        mToolbar = root.findViewById(R.id.buy_toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(mToolbar);
+
 
         return root;
     }
