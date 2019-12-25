c6610f8013e9ccd99d3daa7b845b9c01f607054a
tillchen <ti.chen@jacobs-university.de>
2019-12-25 21:12:25 +0100

    updated iOS BundleName

--- app/src/main/java/com/tillchen/jstore/LoginActivity.java
+++ app/src/main/java/com/tillchen/jstore/LoginActivity.java
@@ -285,7 +285,7 @@ public class LoginActivity extends UtilityActivity implements View.OnClickListen
                         null)
                 .setHandleCodeInApp(true)
                 .setUrl("https://jstore.xyz")
-                .setIOSBundleId("com.tillchen.JStore")
+                .setIOSBundleId("com.tillchen.jstore.ios")
                 .build();
 
         hideKeyboard(mEmailEditText);
